e552677b714d253f750d7033962fb9233fcad340
Michael Myers <michaelzmyers@gmail.com>
2023-05-21 13:11:33 -0500

    Use chain for tile data

--- packages/client/src/mud/createSystemCalls.ts
+++ packages/client/src/mud/createSystemCalls.ts
@@ -1,21 +1,13 @@
-import { getComponentValue } from "@latticexyz/recs";
-import { awaitStreamValue } from "@latticexyz/utils";
-import { ClientComponents } from "./createClientComponents";
 import { SetupNetworkResult } from "./setupNetwork";
 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
-export function createSystemCalls(
-  { worldSend, txReduced$, singletonEntity }: SetupNetworkResult,
-  { Counter }: ClientComponents
-) {
-  const increment = async () => {
-    const tx = await worldSend("increment", []);
-    await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
-    return getComponentValue(Counter, singletonEntity);
-  };
+export function createSystemCalls({ worldSend }: SetupNetworkResult) {
+    const addRandomTile = async (x: number, y: number) => {
+        worldSend("addRandomTile", [x, y]);
+    };
 
-  return {
-    increment,
-  };
+    return {
+        addRandomTile,
+    };
 }
--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -2,59 +2,55 @@ import { mudConfig } from "@latticexyz/world/register";
 
 export default mudConfig({
   enums: {
-    TileType: [
-        'Ground',
-        'Wall',
-        'Door'
-    ],
-    Direction: [
-        'Up',
-        'Left',
-        'Right',
-        'Down'
-    ]
+    TileType: ["Ground", "Wall", "Door"],
+    Direction: ["Up", "Left", "Right", "Down"],
   },
   tables: {
     PositionComponent: {
       schema: {
-        x: 'int32',
-        y: 'int32'
+        x: "int32",
+        y: "int32",
       },
     },
-    GreedComponent: 'uint8',
-    HungerComponent: 'uint8',
-    StaminaComponent: 'uint8',
-    BraveryComponent: 'uint8',
+    GreedComponent: "uint8",
+    HungerComponent: "uint8",
+    StaminaComponent: "uint8",
+    BraveryComponent: "uint8",
     HealthComponent: {
       schema: {
-        current: 'uint16',
-        max: 'uint16'
-      }
-    },
-    DamageComponent: 'uint16',
-    TileComponent: {
-      keySchema: {
-        x: 'int32',
-        y: 'int32'
+        current: "uint16",
+        max: "uint16",
       },
+    },
+    DamageComponent: "uint16",
+    TileComponent: {
+      // keySchema: {
+      //   x: "int8",
+      //   y: "int8",
+      // },
+      // schema: {
+      //   tile: "TileType",
+      // },
       schema: {
-        tile: 'TileType'
-      }
-    }
+        x: "int8",
+        y: "int8",
+        backgroundImage: "string",
+      },
+    },
   },
   systems: {
     MoveSystem: {
-      name: 'Move',
-      openAccess: true
+      name: "Move",
+      openAccess: true,
     },
     PickUpSystem: {
-      name: 'PickUp',
-      openAccess: true
+      name: "PickUp",
+      openAccess: true,
     },
     ExploreSystem: {
-      name: 'Explore',
-      openAccess: true
-    }
+      name: "Explore",
+      openAccess: true,
+    },
   },
   modules: [
     {
