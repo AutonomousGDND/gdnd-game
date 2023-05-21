a6ed0ce265e5e6f28322ab25011e39fdb65d2861
Michael Myers <michaelzmyers@gmail.com>
2023-05-21 13:11:49 -0500

    generated code

--- packages/contracts/src/systems/ExploreSystem.sol
+++ packages/contracts/src/systems/ExploreSystem.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.0;
 
 import { System } from "@latticexyz/world/src/System.sol";
+import { TileComponent } from "../codegen/Tables.sol";
+import { getUniqueEntity } from "@latticexyz/world/src/modules/uniqueentity/getUniqueEntity.sol";
 
 contract ExploreSystem is System {
-
+    function addRandomTile(int8 x, int8 y) public {
+        bytes32 id = getUniqueEntity();
+        TileComponent.set(id, x, y, "bad.png");
+    }
 }
