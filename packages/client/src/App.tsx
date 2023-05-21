dd91ba32eee966fe141ac03645901770d0441da0
Michael Myers <michaelzmyers@gmail.com>
2023-05-21 13:10:38 -0500

    Remove listenOnChain

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,10 @@
 import { PixiProvider } from "./components/PixiProvider";
 import { Grid } from "./components/Grid";
-import listenOnChain from "./datasource/onchainEvents";
 
 export const App = () => {
-    listenOnChain();
     return (
-        <div>
-            <PixiProvider>
-                <Grid />
-            </PixiProvider>
-        </div>
+        <PixiProvider>
+            <Grid />
+        </PixiProvider>
     );
 };
